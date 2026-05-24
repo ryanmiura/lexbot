e849529029cb19f3d7a236b85841cc49d04d785f
ryanmiura <ryanmiuracarrasco@hotmail.com>
2026-05-24 16:44:50 -0300

    feat(config): add environment variable loading

---
+++ config/config.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	DBPath string
+}
+
+func Load() *Config {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("No .env file found or error reading it. Relying on environment variables.")
+	}
+
+	dbPath := os.Getenv("DB_PATH")
+	if dbPath == "" {
+		dbPath = "session.db"
+	}
+
+	return &Config{
+		DBPath: dbPath,
+	}
+}
--- go.mod
+++ go.mod
@@ -14,6 +14,7 @@ require (
 	github.com/coder/websocket v1.8.14 // indirect
 	github.com/elliotchance/orderedmap/v3 v3.1.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/petermattis/goid v0.0.0-20260330135022-df67b199bc81 // indirect
